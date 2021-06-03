149075ab0e57153c089acbc5acb606d3f1c918d5
Anas Ayman <37457417+anass412@users.noreply.github.com>
2021-06-03 23:52:55 +0300

    issue-1:Create project (#4)
    * issue-1:Create project
    
    * issue-1:Remove package-lock.json & add to .gitignore
    
    * issue-1:Change all extensions from .css to .scss
    
    * issue-1:Change all extensions from .css to .scss
    
    * issue-1:Change all extensions from .css to .scss
    
    Co-authored-by: anas <anasaymanissa@gmail.com>

---
+++ src/components/App/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.scss';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './home/home'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
