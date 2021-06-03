0a0f32cd5cde733dcc4f83eebb189be3296deaf5
anas <anasaymanissa@gmail.com>
2021-06-03 22:54:37 +0300

    issue-1:Create project

---
+++ src/components/App/home/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../layout/footer/footer';
+import Header from '../layout/header/header';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+
+      <Footer />
+    </div>
+  );
+}
