27196faef51587287ba7ea96423ac97e5e8a2f10
anas <anasaymanissa@gmail.com>
2021-06-12 23:11:23 +0300

    issue-13:Create Initial Theme Tokens

--- src/components/App/layout/footer/footer.tsx
+++ src/components/App/layout/footer/footer.tsx
@@ -3,6 +3,30 @@ import React from 'react';
 export default function Footer() {
     return (
         <footer>
+        <div className="footer wow fadeIn" data-wow-delay="0.3s">
+            <div className="container-fluid">
+                <div className="container">
+                    <div className="footer-info">
+                        <h2>bla bla bla</h2>
+                        <h3>123 Street, New York, USA</h3>
+                        <div className="footer-menu">
+                            <p>+012 345 67890</p>
+                            <p>info@example.com</p>
+                        </div>
+                        <div className="footer-social">
+                            <a href="https://htmlcodex.com"><i className="fab fa-twitter"></i></a>
+                            <a href="https://htmlcodex.com"><i className="fab fa-facebook-f"></i></a>
+                            <a href="https://htmlcodex.com"><i className="fab fa-youtube"></i></a>
+                            <a href="https://htmlcodex.com"><i className="fab fa-instagram"></i></a>
+                            <a href="https://htmlcodex.com"><i className="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="container copyright">
+                    <p>&copy; <a href="https://htmlcodex.com">Your Site Name</a>, All Right Reserved</p>
+                </div>
+            </div>
+        </div>
         </footer>
     );
 }
--- src/components/App/layout/header/header.tsx
+++ src/components/App/layout/header/header.tsx
@@ -2,7 +2,24 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header>
-    </header>
-  );
+      <div className="navbar navbar-expand-lg bg-light navbar-light">
+            <div className="container-fluid">
+                <a href="index.html" className="navbar-brand">BlaBla</a>
+                <div className="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                    <div className="navbar-nav ml-auto">
+                        <a href="#home" className="nav-item nav-link active">Home</a>
+                        <a href="#about" className="nav-item nav-link">About</a>
+                        <a href="#service" className="nav-item nav-link">Service</a>
+                        <a href="#experience" className="nav-item nav-link">Experience</a>
+                        <a href="#portfolio" className="nav-item nav-link">Portfolio</a>
+                        <a href="#price" className="nav-item nav-link">Price</a>
+                        <a href="#review" className="nav-item nav-link">Review</a>
+                        <a href="#team" className="nav-item nav-link">Team</a>
+                        <a href="#blog" className="nav-item nav-link">Blog</a>
+                        <a href="#contact" className="nav-item nav-link">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
