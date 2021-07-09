3ad6f63af876eecf96bea8f4e14c6eccdce1a383
anas <anasaymanissa@gmail.com>
2021-07-09 10:25:35 +0300

    issue-13: use sass nesting , css BE & remove importantM

--- src/components/App/layout/footer/footer.tsx
+++ src/components/App/layout/footer/footer.tsx
@@ -4,16 +4,16 @@ export default function Footer() {
     return (
         <footer>
         <div className="footer wow fadeIn" data-wow-delay="0.3s">
-            <div className="container-fluid">
-                <div className="container">
-                    <div className="footer-info">
+            <div className="footer__container-fluid">
+                <div className="footer__container">
+                    <div className="footer__footer-info">
                         <h2>bla bla bla</h2>
                         <h3>123 Street, New York, USA</h3>
-                        <div className="footer-menu">
+                        <div className="footer__footer-menu">
                             <p>+012 345 67890</p>
                             <p>info@example.com</p>
                         </div>
-                        <div className="footer-social">
+                        <div className="footer__footer-social">
                             <a href="https://htmlcodex.com"><i className="fab fa-twitter"></i></a>
                             <a href="https://htmlcodex.com"><i className="fab fa-facebook-f"></i></a>
                             <a href="https://htmlcodex.com"><i className="fab fa-youtube"></i></a>
@@ -22,7 +22,7 @@ export default function Footer() {
                         </div>
                     </div>
                 </div>
-                <div className="container copyright">
+                <div className="footer__container footer__copyright">
                     <p>&copy; <a href="https://htmlcodex.com">Your Site Name</a>, All Right Reserved</p>
                 </div>
             </div>
--- src/components/App/layout/header/header.tsx
+++ src/components/App/layout/header/header.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 export default function Header() {
   return (
-      <div className="navbar navbar-expand-lg bg-light navbar-light">
+      <div className="navbar">
             <div className="container-fluid">
-                <a href="index.html" className="navbar-brand">BlaBla</a>
-                <div className="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-                    <div className="navbar-nav ml-auto">
-                        <a href="#home" className="nav-item nav-link active">Home</a>
-                        <a href="#about" className="nav-item nav-link">About</a>
-                        <a href="#service" className="nav-item nav-link">Service</a>
-                        <a href="#experience" className="nav-item nav-link">Experience</a>
-                        <a href="#portfolio" className="nav-item nav-link">Portfolio</a>
-                        <a href="#price" className="nav-item nav-link">Price</a>
-                        <a href="#review" className="nav-item nav-link">Review</a>
-                        <a href="#team" className="nav-item nav-link">Team</a>
-                        <a href="#blog" className="nav-item nav-link">Blog</a>
-                        <a href="#contact" className="nav-item nav-link">Contact</a>
+                <a href="index.html" className="navbar__navbar-brand">BlaBla</a>
+                <div className="navbar__navbar-collapse justify-content-between" id="navbarCollapse">
+                    <div className="navbar__navbar-nav ml-auto">
+                        <a href="#home" className="nav-item navbar__navbar-nav--nav-link active">Home</a>
+                        <a href="#about" className="nav-item navbar__navbar-nav--nav-link">About</a>
+                        <a href="#service" className="nav-item navbar__navbar-nav--nav-link">Service</a>
+                        <a href="#experience" className="nav-item navbar__navbar-nav--nav-link">Experience</a>
+                        <a href="#portfolio" className="nav-item navbar__navbar-nav--nav-link">Portfolio</a>
+                        <a href="#price" className="nav-item navbar__navbar-nav--nav-link">Price</a>
+                        <a href="#review" className="nav-item navbar__navbar-nav--nav-link">Review</a>
+                        <a href="#team" className="nav-item navbar__navbar-nav--nav-link">Team</a>
+                        <a href="#blog" className="nav-item navbar__navbar-nav--nav-link">Blog</a>
+                        <a href="#contact" className="nav-item navbar__navbar-nav--nav-link">Contact</a>
                     </div>
                 </div>
             </div>
