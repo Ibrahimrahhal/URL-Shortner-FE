acbc7889f5c6c8db283469c45815c0ec5c382aee
anas <anasaymanissa@gmail.com>
2021-07-18 03:28:24 +0300

    issue-13: Create Initial Theme Tokens: fixes

--- src/components/App/layout/header/header.tsx
+++ src/components/App/layout/header/header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./header.scss";
 
 export default function Header() {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
